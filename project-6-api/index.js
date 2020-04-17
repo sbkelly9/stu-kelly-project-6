919c8fe1c4224a14706548fbb748ab064e8c41d5
Stu Kelly <sbkelly9@gmail.com>
2020-04-16 22:01:19 -0400

    fixing routes in postman

--- project-6-api/index.js
+++ project-6-api/index.js
@@ -1,20 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const parser = require("body-parser");
-const port = 3000
+const port = 3000;
 const Episodes = require("./models/Episodes");
-
 
 app.use(parser.json());
 
-app.get("/episodes", function(req, res) {
-  Episodes.find({}).then(episodes => {
+app.get("/episodes", function (req, res) {
+  Episodes.find({}).then((episodes) => {
     res.json(episodes);
   });
 });
 
-app.get('/', (req, res) => res.send('music is cool'))
+app.get("/episodes/:id", function (req, res) {
+  Episodes.findOne({ _id: req.params.id }).then((episodes) => {
+    res.json(episodes);
+  });
+});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
+app.post("/episodes", function (req, res) {
+  Episodes.create(req.body).then((episodes) => {
+    res.json(episodes);
+  });
+});
+
+app.post("/episodes/:id", function (req, res) {
+  Episodes.findByIdAndUpdate(
+    req.params.id,
+    { $push: req.body },
+    { new: true }
+  ).then((episodes) => {
+    res.json(episodes);
+  });
+});
+
+app.listen(port, () =>
+  console.log(`Example app episodesening at http://localhost:${port}`)
+);
