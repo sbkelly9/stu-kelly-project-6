db11b6ecea8d3378806cec51bd284ad7906d7cbe
Stu Kelly <sbkelly9@gmail.com>
2020-05-20 18:38:52 -0400

    updated connection file

--- db/connection.js
+++ db/connection.js
@@ -4,4 +4,4 @@ mongoose.connect("mongodb://localhost/episodes", { useNewUrlParser: true });
 
 module.exports = mongoose;
 let mongoURI = "";
-if (process.env.NODE_ENV === "production") {   mongoURI = process.env.DB_URL; } else {   mongoURI = "mongodb://localhost/stu-kelly-project-6-api"; }
+if (process.env.NODE_ENV === "production") {mongoURI = process.env.DB_URL;} else {  mongoURI = "mongodb://localhost/stu-kelly-project-6-api";}
