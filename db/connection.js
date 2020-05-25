4dd865ea201a71042860f49e0d5c1b42281d0067
Stu Kelly <sbkelly9@gmail.com>
2020-05-24 20:04:10 -0400

    changes to connection file

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,11 @@
 let mongoose = require("mongoose");
-let mongoURI = "";
-if (process.env.NODE_ENV === "production") {
-    mongoURI = process.env.DB_URL;
-} else {
-    mongoURI = "mongodb://localhost/stu-kelly-project-6";
-}
-mongoose.connect(mongoURI, { useNewUrlParser: true });
-module.exports = mongoose;
 
+mongoose.Promise = Promise;
+
+let mongoURI = "";
+
+if (process.env.NODE_ENV === "production") {
+  mongoURI = process.env.DB_URL;
+} else {
+  mongoURI = "mongodb://localhost/stu-kelly-project-6";
+}
